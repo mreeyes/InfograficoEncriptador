4534dc1af201c1c568f55854a30c041fbe56298a
mreeyes <{84281641}+{mreeyes}@users.noreply.github.com>
2024-02-21 22:45:12 -0600

    Javascript de encriptado y desencriptado Terminado

--- encriptador.js
+++ encriptador.js
@@ -1,5 +1,5 @@
-let EntradaDeTexto = document.querySelector("#textoEncriptado");
-let BotonEncriptar = document.querySelector("#textoDesencriptado");
+let EntradaDeTexto = document.querySelector("#EntradaTexto");
+let SalidaDeTexto = document.querySelector("#SalidaTexto");
 
 let Lista_Encriptada = [
   ["e", "enter"], //indice 0 (posicion 0 y 1)
@@ -8,16 +8,39 @@ let Lista_Encriptada = [
   ["o", "ober"], //indice 3 (posicion 0 y 1)
   ["u", "ufat"], //indice 4 (posicion 0 y 1)
 ];
+//botones
 function EncriptarTexto() {
-  let texto = Encriptacion(EntradaDeTexto.value);
-  textoDesencriptado.value = texto;
+  let textoDesencriptado = Encriptacion(EntradaDeTexto.value);
+  SalidaDeTexto.value = textoDesencriptado;
 }
 
-function Encriptacion(texto) {
+function DesencriptarTexto() {
+  let textoEncriptado = Descriptacion(EntradaDeTexto.value);
+  SalidaDeTexto.value = textoEncriptado;
+}
+
+//funciones
+function Encriptacion(textoDesencriptado) {
   for (let i = 0; i < Lista_Encriptada.length; i++) {
-    if (texto.includes(Lista_Encriptada[i][0])) {
-      texto = texto.replaceAll(Lista_Encriptada[i][0], Lista_Encriptada[i][1]);
+    if (textoDesencriptado.includes(Lista_Encriptada[i][0])) {
+      //i es el campo de texto escrito por el usuario
+      textoDesencriptado = textoDesencriptado.replaceAll(
+        Lista_Encriptada[i][0],
+        Lista_Encriptada[i][1]
+      );
     }
   }
-  return texto;
+  return textoDesencriptado;
+}
+
+function Descriptacion(textoEncriptado) {
+  for (let i = 0; i < Lista_Encriptada.length; i++) {
+    if (textoEncriptado.includes(Lista_Encriptada[i][1])) {
+      textoEncriptado = textoEncriptado.replaceAll(
+        Lista_Encriptada[i][1],
+        Lista_Encriptada[i][0]
+      );
+    }
+  }
+  return textoEncriptado;
 }
